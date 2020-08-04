bfc74b8a073d6171726b7eace78d101b90b2655a
Luiz Paulo Baçal de Vasconcelos <luizv993@gmail.com>
2020-08-04 17:24:41 -0300

    Desafio 09

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
 
@@ -20,13 +21,71 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+
+    const productsId = products.map(product => ({ id: product.id }));
+    const findProducts = await this.productsRepository.findAllById(productsId);
+
+    if (findProducts.length !== products.length) {
+      throw new AppError('Product not found');
+    }
+
+    const mapProductById = findProducts.reduce(
+      (acc, curr) => ({
+        ...acc,
+        [curr.id]: {
+          price: curr.price,
+          quantity: curr.quantity,
+        },
+      }),
+      {},
+    ) as {
+      [key: string]: {
+        price: number;
+        quantity: number;
+      };
+    };
+
+    const updatedProductsQuantity: IUpdateProductsQuantityDTO[] = products.map(
+      product => {
+        if (product.quantity > mapProductById[product.id].quantity) {
+          throw new AppError(`Product ${product.id} quantity insuficient`);
+        }
+
+        return {
+          id: product.id,
+          quantity: mapProductById[product.id].quantity - product.quantity,
+        };
+      },
+    );
+
+    await this.productsRepository.updateQuantity(updatedProductsQuantity);
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: products.map(product => ({
+        product_id: product.id,
+        quantity: product.quantity,
+        price: mapProductById[product.id].price,
+      })),
+    });
+
+    return order;
   }
 }
 
